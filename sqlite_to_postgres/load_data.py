2e8e30fccd1c41ba95ac4df6828a4616c92d46f7
DavidGQK <davka8989@gmail.com>
2022-10-28 10:10:20 +0300

    db base settings

---
+++ sqlite_to_postgres/load_data.py
@@ -0,0 +1,160 @@
+import logging
+import os
+import sqlite3
+from collections import OrderedDict
+from dataclasses import dataclass
+from os import environ
+from typing import Dict, List, Tuple
+
+import psycopg2
+from dacite import from_dict
+from dotenv import load_dotenv
+from psycopg2.extensions import connection as _connection
+from psycopg2.extras import DictCursor, execute_values
+
+from utils.dclasses import (FilmWork, FilmWorkGenre, FilmWorkPerson, Genre,
+                               Person, sanitize_field)
+
+
+logger = logging.getLogger(__file__)
+logging.basicConfig(filename='load_data.py.log', level=logging.DEBUG)
+
+
+class SQLiteLoader:
+    def __init__(self, connection: sqlite3.Connection,
+                 classes_per_table: Dict[str, dataclass],
+                 tables_names: Tuple[str]) -> None:
+        connection.row_factory = lambda c, r: dict(
+            zip([col[0] for col in c.description], r))
+        self.cursor: connection.cursor = connection.cursor()
+
+        self.classes_per_table = classes_per_table
+        self.tables_names = tables_names
+
+    def _load_table(self, table_name: str) -> List[dataclass]:
+        current_table: List[dataclass] = []
+        self.cursor.execute(f'SELECT * FROM {table_name}')
+
+        current_value_type: dict = self.classes_per_table[
+            table_name].__annotations__.items()
+
+        while True:
+            next_row = self.cursor.fetchone()
+            if next_row:
+                for field_name, field_type in current_value_type:
+                    if next_row[field_name] is None:
+                        del next_row[field_name]
+                    else:
+                        next_row[field_name] = sanitize_field(
+                            field_type=field_type,
+                            field_value=next_row[field_name])
+                current_table.append(
+                    from_dict(self.classes_per_table[table_name], next_row)
+                )
+            else:
+                logger.info(
+                    'Data loaded from table: {}, {} rows'.format(table_name, len(current_table)))
+                return current_table
+
+    def load_movies(self) -> Dict[str, List[dataclass]]:
+        movies_data: Dict[str:List[dataclass]] = {}
+        for table_name in self.tables_names:
+            movies_data[table_name] = self._load_table(table_name)
+        logger.info('All tables were loaded from sqlite')
+        return movies_data
+
+
+class PostgresSaver:
+    def __init__(self, pg_conn: _connection,
+                 page_size: int, schema: str = 'content'):
+        self.cursor = pg_conn.cursor()
+        self.schema = schema
+        self.page_size = page_size
+
+    def _save_data_to_table(self, table_name: str,
+                            table_data: List[dataclass]) -> None:
+        dataclass_fields: Tuple[str] = tuple(
+            table_data[0].__dataclass_fields__.keys())
+        rows_names: str = ', '.join(dataclass_fields)
+
+        rows_for_script: List[tuple] = []
+        for entry in table_data:
+            row: List[str, None] = []
+            for field_name in dataclass_fields:
+                row.append(str(getattr(entry, field_name)) if getattr(entry,
+                                                                      field_name) else None)
+            rows_for_script.append(tuple(row))
+
+        execute_values(
+            cur=self.cursor,
+            sql=f'''INSERT INTO {self.schema}.{table_name} ({rows_names}) 
+                VALUES %s 
+                ON CONFLICT (id) DO NOTHING''',
+            argslist=rows_for_script,
+            page_size=self.page_size)
+        table_size = len(table_data)
+        logger.info(
+            f'Uploaded {table_size} rows for table:{table_name}')
+
+    def save_all_data(self, data: dict, tables: Tuple[str]) -> None:
+        for table_name in tables:
+            self._save_data_to_table(
+                table_name=table_name,
+                table_data=data[table_name])
+
+
+def main():
+    load_dotenv()
+    dsl: Dict[str:str] = {
+        'dbname': environ.get('dbname'),
+        'user': environ.get('user'),
+        # 'password': environ.get('password'),
+        'host': environ.get('host'),
+        'port': environ.get('port'),
+        'options': '-c search_path=content'
+    }
+    sqlite_file: str = environ.get('db_sqlite_file')
+
+    classes_per_table: OrderedDict = OrderedDict(
+        [('film_work', FilmWork),
+         ('genre', Genre),
+         ('person', Person),
+         ('genre_film_work', FilmWorkGenre),
+         ('person_film_work', FilmWorkPerson)]
+    )
+    tables_names: Tuple[str] = tuple(map(str, classes_per_table.keys()))
+
+    if not os.path.isfile(sqlite_file):
+        raise OSError('The sqlite file does not exist')
+
+    try:
+        with sqlite3.connect(sqlite_file) as sqlite_conn:
+            sqlite_loader: SQLiteLoader = SQLiteLoader(
+                connection=sqlite_conn,
+                classes_per_table=classes_per_table,
+                tables_names=tables_names)
+            movies_data: Dict[str:List[dataclass]] = sqlite_loader.load_movies()
+    except sqlite3.OperationalError as ex:
+        logger.exception(ex)
+        print('shit sqlite3 ' * 10)
+    finally:
+        sqlite_conn.close()
+
+    try:
+        with psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+            postgres_saver: PostgresSaver = PostgresSaver(
+                pg_conn=pg_conn,
+                page_size=int(environ.get('page_size')),
+                schema=environ.get('schema'))
+            postgres_saver.save_all_data(data=movies_data,
+                                         tables=tables_names)
+    except psycopg2.Error as e:
+        logger.exception(e.pgerror)
+    finally:
+        pg_conn.close()
+
+    logger.info('All tasks have worked correctly')
+
+
+if __name__ == '__main__':
+    main()
---
+++ sqlite_to_postgres/utils/dclasses.py
@@ -0,0 +1,68 @@
+import uuid
+from dataclasses import dataclass, field, fields
+from datetime import datetime
+from typing import List, Type, Union
+from dateutil import parser
+
+
+@dataclass
+class FilmWork:
+    title: str
+    description: str = None
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    creation_date: str = None
+    certificate: str = None
+    file_path: str = None
+    rating: float = field(default=0.0)
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
+    type: str = field(default="movie")
+
+
+@dataclass
+class Genre:
+    name: str
+    description: str = None
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
+
+
+@dataclass
+class Person:
+    full_name: str
+    birth_date: str = None
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+
+
+@dataclass
+class FilmWorkPerson:
+    film_work_id: uuid.UUID
+    person_id: uuid.UUID
+    role: str = None
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    created_at: datetime = field(default_factory=datetime.now)
+
+
+@dataclass
+class FilmWorkGenre:
+    film_work_id: uuid.UUID
+    genre_id: uuid.UUID
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    created_at: datetime = field(default_factory=datetime.now)
+
+
+fields_types: List[Type[
+    Union[str, float, datetime, uuid.UUID]]] = [str, float, datetime, uuid.UUID]
+
+def sanitize_field(
+        field_type: fields_types, field_value: str) -> fields_types:
+    dict_type_function = {
+        float: float,
+        uuid.UUID: uuid.UUID,
+        str: lambda x: x.replace("'", "''"),
+        datetime: parser.isoparse
+    }
+    return dict_type_function[field_type](field_value)
